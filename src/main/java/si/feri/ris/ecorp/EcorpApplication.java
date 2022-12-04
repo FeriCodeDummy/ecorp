cd3b63365b73b83c8afb11e172b5f04053831f35
FeriCodeDummy <jan.zajc3@student.um.si>
2022-12-04 12:11:50 +0100

    Initial commit

---
+++ src/main/java/si/feri/ris/ecorp/EcorpApplication.java
@@ -0,0 +1,13 @@
+package si.feri.ris.ecorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcorpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/feri/ris/ecorp/EcorpApplicationTests.java
@@ -0,0 +1,13 @@
+package si.feri.ris.ecorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcorpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
